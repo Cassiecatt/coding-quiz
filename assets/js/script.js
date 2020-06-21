e01409db35b04134bed7d0bc702b5279d3f1f62d
cassie.cattabiani@gmail.com <cassie.cattabiani@gmail.com>
2020-06-21 09:59:27 -0700

    built question array

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+// Array of questions
+
+var questions = [
+  {
+    question: "Commonly used data types DO not include:",
+    choice1: "A: String",
+    choice2: "B: Booleans",
+    choice3: "C: Alerts",
+    choice4: "D: Numbers",
+    answer: "choice1",
+  },
+
+  {
+    question: "The condition in an if / else statement is enclosed with:",
+    choice1: "A: Quotes",
+    choice2: "B: Curly Brackets",
+    choice3: "C: Parenthesis",
+    choice4: "D: Square Brackets",
+    answer: "choice3",
+  },
+
+  {
+    question: "Arrays in javascript can be used to store:",
+    choice1: "A: Numbers and strings",
+    choice2: "B: Other arrays",
+    choice3: "C: Booleans",
+    choice4: "D: All of the above",
+    answer: "choice4",
+  },
+
+  {
+    question: "String values must be enclosed within ___ when being assigned to variables.",
+    choice1: "A: Commas",
+    choice2: "B: Curley brackets",
+    choice3: "C: Quotes",
+    choice4: "D: Parenthesis",
+    answer: "choice2",
+  },
+
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choice1: "A: Javascript",
+    choice2: "B: Terminal/bash",
+    choice3: "C: For loops",
+    choice4: "D: console.log",
+    answer: "choice3",
+  },
+
+];
